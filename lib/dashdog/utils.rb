02a4cbc32e2becea94da94fae27622696ff17952
Genki Sugawara <sugawara@cookpad.com>
2016-08-22 17:53:33 +0900

    Diff json after sort

--- lib/dashdog/utils.rb
+++ lib/dashdog/utils.rb
@@ -7,8 +7,8 @@ module Dashdog
 
     def self.diff(hash1, hash2)
       Diffy::Diff.new(
-        JSON.pretty_generate(hash1) + "\n",
-        JSON.pretty_generate(hash2) + "\n",
+        JSON.pretty_generate(self.deep_sort_hash(hash1)) + "\n",
+        JSON.pretty_generate(self.deep_sort_hash(hash2)) + "\n",
         :diff => '-u'
       ).to_s(:color)
     end
@@ -30,6 +30,25 @@ module Dashdog
 
     def self.print_json(json)
       puts CodeRay.scan(json, :json).terminal
+    end
+
+    def self.deep_sort_hash(obj)
+      case obj
+      when Hash
+        new_hash = {}
+
+        obj.sort_by{|k, _| k.to_s }.each do |key, value|
+          new_hash[key] = self.deep_sort_hash(value)
+        end
+
+        new_hash
+      when Array
+        obj.map do |value|
+          self.deep_sort_hash(value)
+        end
+      else
+        obj
+      end
     end
   end
 end
